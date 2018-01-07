d17d9b605340c85c08e3ad8bc3d2c0c467d7a658
CycloPi <venti3d@gmail.com>
2018-01-07 22:49:51 +0100

    Actualizo HUb terrreno

--- Crazy_Tron_Car/Pistas/HUD/HUD.pista.gd
+++ Crazy_Tron_Car/Pistas/HUD/HUD.pista.gd
@@ -1,7 +1,26 @@
 extends Control
 
 
-onready var coche = Global.coche.get_node("BODY")
+var direccionArchivo = "res://infojugador.json"
+var circuitoSeleccionado
+
+
+
+var cocheSeleccionado
+var fechaPartida
+var diccionarioPartida = {"coche": cocheSeleccionado,"circuito": circuitoSeleccionado,"fecha": fechaPartida}
+var diccionarioPaGuardar = {"key1": diccionarioPartida}
+
+
+# variables guardadas
+var diccionarioUltimaPartida
+
+# variables info cicloPi
+onready var imprimeInfo = get_node("Ultima")
+onready var imprimePartidaActual = get_node("info")
+
+
+
 #variables para relog
 #onready var imprimeTiempo = get_node("SpriteTiempo/Tiempo")
 var tiempoInicio = 0
@@ -9,18 +28,33 @@ var banderaInicioT = false
 var segundosT = 0
 var vueltas = 0 
 #variables para boqueo Salida
+onready var coche = Global.coche.get_node("BODY")
 onready var fuerzaMotor = coche.engine_force
 var banderaEnCarrera = false
 onready var cuentaAtras = get_node("cuentaAtras")
 
 func _ready():
 #	print (Global.coche.get_node("BODY").get_name())
-
 	set_fixed_process(true)
 #	print("coche " + str(coche.get_name()))
+	leerJuego()
+	print(Global.pista)
+	#print(get_parent().get_name())
+
+	print ("leo ultima: ",diccionarioUltimaPartida)
 	
-
-
+	# leer datos actuales de partida 
+	cocheSeleccionado = coche.get_child(0).get_name() #paguardar nombre circuito
+	circuitoSeleccionado = Global.pista #guardar nobre ccoche
+	fechaPartida = OS.get_date(true) #guardar fecha partida
+	diccionarioPartida  = {"coche": cocheSeleccionado,"circuito": circuitoSeleccionado,"fecha": fechaPartida}
+	#guardopartida datos actuales de partida 
+	diccionarioPaGuardar = {"key1": diccionarioPartida}
+	
+	guardarJuego(diccionarioPaGuardar)
+	imprimeInfo.set_text("Anterior" + str(diccionarioUltimaPartida))
+	imprimePartidaActual.set_text(("Actual" + str(diccionarioPaGuardar)))
+#	if (get_name() == str("truckCicloPi")): # el if creo que no hace falta no se si ponerlo
 	pass
 	
 func _fixed_process(delta):
@@ -81,3 +115,23 @@ func _on_Back_pressed():
 	get_tree().get_root().get_node("carrera").queue_free()
 	get_tree().change_scene("res://Menus/M-Principal/base.tscn")
 
+# funciones de guardar
+func guardarJuego(diccionarioGuardar):	
+	
+	var archivoGuardar = File.new()
+	archivoGuardar.open(direccionArchivo, archivoGuardar.WRITE)
+	archivoGuardar.store_line(diccionarioGuardar.to_json())
+	archivoGuardar.close()	
+	print("guardo")
+
+func leerJuego():  
+	var archivoLeer = File.new()
+	if not archivoLeer.file_exists(direccionArchivo):
+		return
+	
+	archivoLeer.open(direccionArchivo, File.READ)
+	var diccionarioLeido = {}
+	diccionarioLeido.parse_json(archivoLeer.get_as_text())
+	print(diccionarioLeido.key1)
+	diccionarioUltimaPartida = diccionarioLeido.key1
+	print("leeeo")
