55b8366797297d57667abea0afa694c0322f8430
brecabre <bravomantonio@gmail.com>
2018-01-14 17:59:20 +0100

    cambio selección pista para añadir el fantasma

--- Crazy_Tron_Car/Menus/SeleccionPista/SeleccionPista.gd
+++ Crazy_Tron_Car/Menus/SeleccionPista/SeleccionPista.gd
@@ -2,16 +2,29 @@ extends Control
 
 var carrera 
 
-func _load_scene(road):
+
+
+func _load_scene(road,fantasma_escena):
 	carrera = load(road).instance()
 	Global.pista = carrera.get_name()
 	carrera.set_name("carrera")
-	print ("pista seleccionada: " + str(road))
+#	print ("pista seleccionada: " + str(road))
 #	print("pista "+ str(carrera))
 	carrera.get_node("PosicionSalida").add_child(Global.coche)
 	get_parent().add_child(carrera)
 	
+#	var fantasma = 
+#	get_parent().add_child(fantasma)
+	carrera.get_node("PosicionSalida").add_child(load(fantasma_escena).instance())
+	
+
 	get_tree().get_root().get_node("SeleccionPista").queue_free()
+
+### carga el diccionario y instancia la escena del fantasma ###
+
+	
+	
+	
 	
 # Atras Seleccion de coche
 func _on_Back_pressed():
@@ -26,16 +39,17 @@ func _on_MenuPrincipal_pressed():
 
 # Seleccionar pista Truck Town
 func _on_Pista_2_pressed():
-	_load_scene("res://Pistas/TruckTown/truck_scene.scn")
-
+	_load_scene("res://Pistas/TruckTown/truck_scene.scn","res://Coches/fantasma/fantasma.tscn")
+	
 # Seleccionar pista Jarama
 func _on_Pista_1_pressed():
-	_load_scene("res://Pistas/Jarama/jarama_scene.scn")
-
+	_load_scene("res://Pistas/Jarama/jarama_scene.scn","res://Coches/fantasma/fantasma.tscn")
+	
 # Seleccionar pista Rosco Campero
 func _on_Pista3_pressed():
-	_load_scene("res://Pistas/RoscoCampero/RoscoCampero_Scene.tscn")
+	_load_scene("res://Pistas/RoscoCampero/RoscoCampero_Scene.tscn","res://Coches/fantasma/fantasma.tscn")
 	
 # Seleccionar pista Platano
 func _on_Pista4_pressed():
-	_load_scene("res://Pistas/circuito_platano/terreno.tscn")
+	_load_scene("res://Pistas/circuito_platano/terreno.tscn","res://Coches/fantasma/fantasma.tscn")
+	
