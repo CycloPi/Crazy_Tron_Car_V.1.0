bd762beb0d5a683c54f4e6b504c061d7a8d393e7
brecabre <bravomantonio@gmail.com>
2018-02-06 21:55:01 +0100

    fantasmica para todos los terrenos

--- Crazy_Tron_Car/Coches/fantasma/fantasma.gd
+++ Crazy_Tron_Car/Coches/fantasma/fantasma.gd
@@ -2,11 +2,10 @@ extends Spatial
 
 onready var tiempo 
 var numero_frame = 0
-### detecto pista ####
 #onready var circuito = get_tree().get_root().get_node("/root/carrera/HUDpista")
 onready var circuito = Global.pista
+onready var HUDpista = get_tree().get_root().get_node("/root/carrera/HUDpista")
 
-#onready var circuito = HUB.circuitoSeleccionado
 onready var  meta = get_node("meta")
 onready var  parcial1 = get_node("parcial1")
 onready var  parcial2 = get_node("parcial2")
@@ -33,8 +32,6 @@ onready var  metaVolanteParcial10 = get_node("parcial10/metaVolante")
 var vueltas = 0
 
 ############ posiciones coche ###########################################
-
-
 var	posicion_coche_x_0 = []
 var	posicion_coche_x_1 = []
 var	posicion_coche_x_2 = []
@@ -51,6 +48,7 @@ var origen_coche_0 = []
 var origen_coche_1 = []
 var origen_coche_2 = []
 
+### posiciones circuito####
 
 var posicion_fantasma_terreno_x_0 = []
 var posicion_fantasma_terreno_x_1 = []
@@ -118,8 +116,7 @@ var origen_fantasma_town_scene_0 = []
 var origen_fantasma_town_scene_1 = []
 var origen_fantasma_town_scene_2 = []
 
-	
-## para que se mueva el cubo y no el nodo principal ###
+
 onready var fantasma_body = get_node("fantasma")
 var direccion = "res://fantasma.json"
 
@@ -201,7 +198,6 @@ func salvar():
 	file.open(direccion, file.WRITE)
 	file.store_line(salvar.to_json())
 	file.close()
-#	
 	
 func load_game(): 
 	var file = File.new()
@@ -284,25 +280,23 @@ func load_game():
 	origen_fantasma_town_scene_1 = data.dic.origen_fantasma_town_scene_1
 	origen_fantasma_town_scene_2 = data.dic.origen_fantasma_town_scene_2
 	
-	
 
 func _ready():
 
 	load_game()
-	#### #### escone la meta del coche, esconde la leyermask (.hide no sive) ####
-	meta.set_layer_mask_bit(0,false)
-	parcial1.set_layer_mask_bit(0,true)
-	parcial2.set_layer_mask_bit(0,false)
-	parcial3.set_layer_mask_bit(0,false)
-	parcial4.set_layer_mask_bit(0,false)
-	parcial5.set_layer_mask_bit(0,false)
-	parcial6.set_layer_mask_bit(0,false)
-	parcial7.set_layer_mask_bit(0,false)
-	parcial8.set_layer_mask_bit(0,false)
-	parcial9.set_layer_mask_bit(0,false)
-	parcial10.set_layer_mask_bit(0,false)
+	iniciar_parciales()
+	iniciar_metasVolantes()
+	grabar_coche()
+	set_fixed_process(true)
 	
-	metaVolanteParcial1.show()
+	
+	
+	
+	
+func iniciar_metasVolantes():
+#	hace que se vean los arcos de las metasVolantes
+#	metaVolanteMeta.hide()
+#	metaVolanteParcial1.show()
 	metaVolanteParcial2.hide()
 	metaVolanteParcial3.hide()
 	metaVolanteParcial4.hide()
@@ -313,14 +307,21 @@ func _ready():
 	metaVolanteParcial9.hide()
 	metaVolanteParcial10.hide()
 	
-	
-	grabar_coche()
-	
-	set_fixed_process(true)
-	
+func iniciar_parciales():
+	#### #### escone la meta del coche, esconde la leyermask (.hide no se ve) ####
+	meta.set_layer_mask_bit(0,false)
+	parcial1.set_layer_mask_bit(0,false)
+	parcial2.set_layer_mask_bit(0,false)
+	parcial3.set_layer_mask_bit(0,false)
+	parcial4.set_layer_mask_bit(0,false)
+	parcial5.set_layer_mask_bit(0,false)
+	parcial6.set_layer_mask_bit(0,false)
+	parcial7.set_layer_mask_bit(0,false)
+	parcial8.set_layer_mask_bit(0,false)
+	parcial9.set_layer_mask_bit(0,false)
+	parcial10.set_layer_mask_bit(0,false)
+	pass
 
-
-### hace correr al fantasma recorriendo las listas del diccionario con "numero_frame" ###
 func fantasma_corre():
 #	var nodo_fantasma = get_tree().get_root().get_node("fantasma")
 #	# cambiar
@@ -384,7 +385,6 @@ func fantasma_corre():
 				pass
 			else: numero_frame +=1
 
-
 func grabar_coche():	
 	### apendiza a las listas del diccionario la matrix3x4 ####
 	var coche_nodo = get_tree().get_root().get_node("carrera/PosicionSalida/car/BODY")
@@ -405,17 +405,16 @@ func grabar_coche():
 	origen_coche_1.append(coche_nodo.get_global_transform().origin[1])
 	origen_coche_2.append(coche_nodo.get_global_transform().origin[2])
 	
-	
 func _fixed_process(delta):
-	
-#	print(circuito.circuitoSeleccionado)
-	
 	tiempo =  get_tree().get_root().get_node("/root/carrera/HUDpista").segundosT
-	if tiempo>=3:  
+	if tiempo>=3:
 		grabar_coche()
 		fantasma_corre()
 #	print("tiempo:",tiempo)
 # area que hace que deje de grabar las posiciones del coche  "meta"
+
+	
+
 func _on_meta_body_enter( BODY ):
 	if circuito == "terreno":
 		if posicion_fantasma_terreno_x_0.empty() or posicion_coche_x_0.size() < posicion_fantasma_terreno_x_0.size():
@@ -473,20 +472,25 @@ func _on_meta_body_enter( BODY ):
 	parcial1.set_layer_mask_bit(0,true)
 #	meta.set_layer_mask_bit(0,false)
 #	metaVolanteParcial3.hide()
-	metaVolanteParcial1.show()
+#	
+#	metaVolanteParcial1.show()
+	HUDpista.metaVolante.hide()
+	HUDpista.parcial1Volante.show()
+	
 	print ("meta")
+	print ("Acho aqui solo entra una vez")
 	vueltas += 1
 	print ("vueltas: " + str(vueltas))
 	
 
 func _on_parcial1_body_enter( BODY ):
-	#### hace vidible  la meta del coche, hace visible la leyermask  ####
+	#### hace vidible  la meta del coche, hace visible la layermask  ####
 	parcial2.set_layer_mask_bit(0,true)
 	meta.set_layer_mask_bit(0,false)
-#	parcial1.set_layer_mask_bit(0,false)  
-	metaVolanteParcial1.hide()
+#	metaVolanteParcial1.hide()
 	metaVolanteParcial2.show()
-	   
+	HUDpista.parcial1Volante.hide()
+	HUDpista.parcial2Volante.show()
 	print ("parcial1")
 	pass
 func _on_parcial2_body_enter( BODY ):
@@ -494,81 +498,92 @@ func _on_parcial2_body_enter( BODY ):
 	parcial1.set_layer_mask_bit(0,false)
 	metaVolanteParcial2.hide()
 	metaVolanteParcial3.show()
-#	parcial2.set_layer_mask_bit(0,false)
+	HUDpista.parcial2Volante.hide()
+	HUDpista.parcial3Volante.show()
 	print ("parcial2")
-	pass # replace with function body
+	pass 
 	
 func _on_parcial3_body_enter( BODY ):
 	parcial4.set_layer_mask_bit(0,true)
-#	parcial3.set_layer_mask_bit(0,false)
 	parcial2.set_layer_mask_bit(0,false)
 	metaVolanteParcial3.hide()
 	metaVolanteParcial4.show()
+	HUDpista.parcial3Volante.hide()
+	HUDpista.parcial4Volante.show()
 	print ("parcial3")
-	pass # replace with function body
+	pass 
 	
 func _on_parcial4_body_enter( BODY ):
 	parcial5.set_layer_mask_bit(0,true)
-#	parcial4.set_layer_mask_bit(0,false)
 	parcial3.set_layer_mask_bit(0,false)
 	metaVolanteParcial4.hide()
 	metaVolanteParcial5.show()
+	HUDpista.parcial4Volante.hide()
+	HUDpista.parcial5Volante.show()
 	print ("parcial4")
-	pass # replace with function body
+	pass 
 	
 func _on_parcial5_body_enter( BODY ):
 	parcial6.set_layer_mask_bit(0,true)
-#	parcial5.set_layer_mask_bit(0,false)
 	parcial4.set_layer_mask_bit(0,false)
 	metaVolanteParcial5.hide()
 	metaVolanteParcial6.show()
+	HUDpista.parcial5Volante.hide()
+	HUDpista.parcial6Volante.show()
 	print ("parcial5")
-	pass # replace with function body
+	pass 
 	
 func _on_parcial6_body_enter( BODY ):
 	parcial7.set_layer_mask_bit(0,true)
-#	parcial6.set_layer_mask_bit(0,false)
 	parcial5.set_layer_mask_bit(0,false)
 	metaVolanteParcial6.hide()
 	metaVolanteParcial7.show()
+	HUDpista.parcial6Volante.hide()
+	HUDpista.parcial7Volante.show()
 	print ("parcial6")
-	pass # replace with function body
+	pass 
 	
 func _on_parcial7_body_enter( BODY ):
 	parcial8.set_layer_mask_bit(0,true)
-#	parcial7.set_layer_mask_bit(0,false)
 	parcial6.set_layer_mask_bit(0,false)
 	metaVolanteParcial7.hide()
 	metaVolanteParcial8.show()
+	HUDpista.parcial7Volante.hide()
+	HUDpista.parcial8Volante.show()
 	print ("parcial7")
-	pass # replace with function body
+	pass 
 	
 func _on_parcial8_body_enter( BODY ):
 	parcial9.set_layer_mask_bit(0,true)
-#	parcial8.set_layer_mask_bit(0,false)
 	parcial7.set_layer_mask_bit(0,false)
 	metaVolanteParcial8.hide()
 	metaVolanteParcial9.show()
+	HUDpista.parcial8Volante.hide()
+	HUDpista.parcial9Volante.show()
 	print ("parcial8")
-	pass # replace with function body
+	pass 
 	
 func _on_parcial9_body_enter( BODY ):
 	parcial10.set_layer_mask_bit(0,true)
-#	parcial9.set_layer_mask_bit(0,false)
 	parcial8.set_layer_mask_bit(0,false)
 	metaVolanteParcial9.hide()
 	metaVolanteParcial10.show()
+	HUDpista.parcial9Volante.hide()
+	HUDpista.parcial10Volante.show()
 	print ("parcial9")
-	pass # replace with function body
+	pass 
+	
 	
 func _on_parcial10_body_enter( BODY ):
-	meta.set_layer_mask_bit(0,true)
 #	parcial10.set_layer_mask_bit(0,false)
+	meta.set_layer_mask_bit(0,true)
 	parcial9.set_layer_mask_bit(0,false)
 	metaVolanteParcial10.hide()
 	metaVolanteMeta.show()
+	HUDpista.parcial10Volante.hide()
+	HUDpista.metaVolante.show()
 	print ("parcial10")
-	pass # replace with function body
+	pass 
 
 
 
--- Crazy_Tron_Car/Pistas/HUD/HUD.pista.gd
+++ Crazy_Tron_Car/Pistas/HUD/HUD.pista.gd
@@ -1,28 +1,34 @@
 extends Control
 
-
+# variables guardadas
+var diccionarioUltimaPartida
 var direccionArchivo = "res://infojugador.json"
 var circuitoSeleccionado
-
 var archivoLeer 
+
 #variables para las vueltas
 var tiempoVuelta = 0
 var cocheSeleccionado
 var fechaPartida
-var diccionarioPartida = {"coche": cocheSeleccionado,"circuito": circuitoSeleccionado,"fecha": fechaPartida,"tiempoVuelta": tiempoVuelta}
-var diccionarioPaGuardar = {"key1": diccionarioPartida}
-
-
-# variables guardadas
-var diccionarioUltimaPartida
+var diccionarioPartida = {"coche": cocheSeleccionado,"circuito": circuitoSeleccionado,"fecha": fechaPartida}
+var diccionarioTiempos = {"Tvuela": tiempoVuelta}
+var diccionarioUltimosTiempos
+var diccionarioPaGuardar = {"partida": diccionarioPartida, "tiempos":diccionarioTiempos}
 
 # variables info cicloPi
 onready var imprimeInfo = get_node("Ultima")
 onready var imprimePartidaActual = get_node("info")
 
+# var para marcador de tiempos:
+var BanderaMarcador = false
+var preEsceMarcador = preload("res://Pistas/HUD/MarcadorRetro/MarcadoRetroEscena.tscn")
+var MarcadoTiempo
+
+var InicioTempo3s = 0
 
 
-#variables para relog
+
+#variables para relog 
 #onready var imprimeTiempo = get_node("SpriteTiempo/Tiempo")
 var tiempoInicio = 0
 var banderaInicioT = false
@@ -41,44 +47,32 @@ onready var cuentaAtras = get_node("cuentaAtras")
 #onready var cocheMiniMapa = get_tree().get_root().get_node("/root/carrera/PosicionSalida/car")
 onready var fantasma = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/fantasma")
 onready var fantasmika = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma")
-onready var meta = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/meta/metaVolante")
-onready var parcial1 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial1/posMetaVolante")
-onready var parcial2 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial2/posMetaVolante")
-onready var parcial3 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial3/posMetaVolante")
-onready var parcial4 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial4/posMetaVolante")
-onready var parcial5 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial5/posMetaVolante")
-onready var parcial6 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial6/posMetaVolante")
-onready var parcial7 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial7/posMetaVolante")
-onready var parcial8 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial8/posMetaVolante")
-onready var parcial9 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial9/posMetaVolante")
-onready var parcial10 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial10/posMetaVolante")
+
+onready var meta = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/meta")
+onready var parcial1 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial1")
+onready var parcial2 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial2")
+onready var parcial3 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial3")
+onready var parcial4 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial4")
+onready var parcial5 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial5")
+onready var parcial6 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial6")
+onready var parcial7 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial7")
+onready var parcial8 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial8")
+onready var parcial9 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial9")
+onready var parcial10 = get_tree().get_root().get_node("/root/carrera/PosicionSalida/fantasma/parcial10")
 
 
+onready var parcial1Volante = get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante1")
+onready var parcial2Volante = get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante2")
+onready var parcial3Volante = get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante3")
+onready var parcial4Volante = get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante4")
+onready var parcial5Volante = get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante5")
+onready var parcial6Volante = get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante6")
+onready var parcial7Volante = get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante7")
+onready var parcial8Volante = get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante8")
+onready var parcial9Volante = get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante9")
+onready var parcial10Volante = get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante10")
+onready var metaVolante = get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante")
 
-func _ready():
-
-	set_fixed_process(true)
-	#   Funciones CycloPi
-	leerJuego()
-	datosPartida()
-	guardarJuego(diccionarioPaGuardar)
-	print(parcial1)
-	pass
-	
-func _fixed_process(delta):
-	relogiko()
-	salidaFuerza()
-	cuentaAtras()
-	tiempo_vueltas()
-	posicion_miniMapa()
-	
-	if fantasmika.vueltas > vueltas:
-		vueltas += 1
-		print("meto paso por meta")
-	
-	pass
-	
-	
 func posicion_miniMapa():
 	#print (posicion0Coche)
 	#print (cocheMiniMapa.get_name())
@@ -86,7 +80,12 @@ func posicion_miniMapa():
 	var CameraMiniMapa = get_node("MiniMapa/Viewport/CameraMiniMapa")
 	var cochePos = CameraMiniMapa.unproject_position(coche.get_transform().origin)
 	var fantasmaPos = CameraMiniMapa.unproject_position(fantasma.get_transform().origin)
-	
+	var cocheRoty = coche.get_rotation_deg().y
+#	print ("Rot Y: " + str(cocheRoty))
+	var cocheRotx = coche.get_rotation_deg().x
+#	print ("Rot X: " + str(cocheRotx))
+	var cocheRotz = coche.get_rotation_deg().z
+#	print ("Rot Z: " + str(cocheRotz))
 	var metaPos = CameraMiniMapa.unproject_position(meta.get_transform().origin)
 	var parcial1Pos = CameraMiniMapa.unproject_position(parcial1.get_transform().origin)
 	var parcial2Pos = CameraMiniMapa.unproject_position(parcial2.get_transform().origin)
@@ -98,24 +97,79 @@ func posicion_miniMapa():
 	var parcial8Pos = CameraMiniMapa.unproject_position(parcial8.get_transform().origin)
 	var parcial9Pos = CameraMiniMapa.unproject_position(parcial9.get_transform().origin)
 	var parcial10Pos = CameraMiniMapa.unproject_position(parcial10.get_transform().origin)
-	
-	print (get_node("MiniMapa/Viewport/CameraMiniMapa").unproject_position(parcial1.get_transform().origin))
-	print (str(parcial1) + str(parcial1.get_tree()) + str(parcial1.get_filename())) 
+
+	if cocheRotx > -99:
+		get_node("MiniMapa/Viewport/CameraMiniMapa/coche").set_rotd(cocheRoty + 180)
+	else:
+				get_node("MiniMapa/Viewport/CameraMiniMapa/coche").set_rotd(-cocheRoty)
+	#print (get_node("MiniMapa/Viewport/CameraMiniMapa").unproject_position(coche.get_transform().origin))
+#	print (str(fantasma) + str(fantasma.get_tree()) + str(fantasma.get_filename())) 
 	get_node("MiniMapa/Viewport/CameraMiniMapa/coche").set_pos(cochePos)
+	
 	get_node("MiniMapa/Viewport/CameraMiniMapa/fantasma").set_pos(fantasmaPos)
-	get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante").set_pos(metaPos)
-	get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante").set_pos(parcial1Pos)
-	get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante").set_pos(parcial2Pos)
-	get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante").set_pos(parcial3Pos)
-	get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante").set_pos(parcial4Pos)
-	get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante").set_pos(parcial5Pos)
-	get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante").set_pos(parcial6Pos)
-	get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante").set_pos(parcial7Pos)
-	get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante").set_pos(parcial8Pos)
-	get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante").set_pos(parcial9Pos)
-	get_node("MiniMapa/Viewport/CameraMiniMapa/metaVolante").set_pos(parcial10Pos)
+
+	parcial1Volante.set_pos(Vector2(-20,-20))
+	parcial2Volante.set_pos(parcial2Pos)
+	parcial3Volante.set_pos(parcial3Pos)
+	parcial4Volante.set_pos(parcial4Pos)
+	parcial5Volante.set_pos(parcial5Pos)
+	parcial6Volante.set_pos(parcial6Pos)
+	parcial7Volante.set_pos(parcial7Pos)
+	parcial8Volante.set_pos(parcial8Pos)
+	parcial9Volante.set_pos(parcial9Pos)
+	parcial10Volante.set_pos(parcial10Pos)
+	metaVolante.set_pos(metaPos)
+	
+	
+#	metaVolante.hide()
+#	parcial1Volante.hide()
+#	parcial2Volante.hide()
+#	parcial3Volante.hide()
+#	parcial4Volante.hide()
+#	parcial5Volante.hide()
+#	parcial6Volante.hide()
+#	parcial7Volante.hide()
+#	parcial8Volante.hide()
+#	parcial9Volante.hide()
+#	parcial10Volante.hide()
+	pass
+
+
+
+func _ready():
+
+	set_fixed_process(true)
+	#   Funciones CycloPi
+	leerJuego()
+	datosPartida()
+	guardarJuego(diccionarioPaGuardar)
+	pass
+	
+func _fixed_process(delta):
+	relogiko()
+	salidaFuerza()
+	cuentaAtras()
+	tiempo_vueltas()
+	posicion_miniMapa()
+	#funcion en pruebas:
+	# No no no funciona el contador de vueltas del fantasma
+	# solo marca la primera: 
+	#
+	#print(fantasmika.vueltas)
+	#
+	# Solo cuenta una vez , No entra en el area lo he emirado
+	# grabar tiempo
+	grabarTiempos()
+	
+	# funcion salir de temporzador y matar el marcador
+	if segundosT - InicioTempo3s == 4 and BanderaMarcador :
+		MarcadoTiempo.free()
+		BanderaMarcador = false
+		print("ahora han pasado 4 segundos")
 	
 	pass
+	
+	
 
 func tiempo_vueltas():
 	var tiempoveltas
@@ -151,6 +205,28 @@ func salidaFuerza():
 	if segundosT == 3:
 		coche.engine_force = 100
 		banderaEnCarrera = true
+		
+		
+		# funcion para sacar el marcador
+		if BanderaMarcador == false:
+			print("saca el cartel último tiempo")
+			MarcadoTiempo = preEsceMarcador.instance()
+			get_parent().add_child(MarcadoTiempo)
+			MarcadoTiempo.set_pos(Vector2(0,200))
+			BanderaMarcador = true
+			InicioTempo3s = segundosT
+			print(diccionarioUltimosTiempos)
+			MarcadoTiempo.get_node("Informacion").set_text("ÚT: "+str(diccionarioUltimosTiempos.Tvuela)+" s")
+#			MarcadoTiempo.get_node("Informacion").set_text("Wellcome")
+			
+			
+			if not archivoLeer.file_exists(direccionArchivo):
+				MarcadoTiempo.get_node("Informacion").set_text("Wellcome")
+#				imprimeInfo.set_text("Bienvenido" )
+
+#			else:
+#				MarcadoTiempo.get_node("Informacion").set_text("ÚT: "+str(diccionarioUltimosTiempos.Tvuela)+" s")
+	
 	if segundosT < 3 and banderaEnCarrera == false:
 		coche.engine_force = 0
 		
@@ -181,16 +257,22 @@ func leerJuego():
 	archivoLeer.open(direccionArchivo, File.READ)
 	var diccionarioLeido = {}
 	diccionarioLeido.parse_json(archivoLeer.get_as_text())
-#	print(diccionarioLeido.key1)
-	diccionarioUltimaPartida = diccionarioLeido.key1
+#	print(diccionarioLeido.key1p)
+	diccionarioUltimaPartida = diccionarioLeido.partida
+	print("tiempoleido")
+	diccionarioUltimosTiempos = diccionarioLeido.tiempos
+	print(diccionarioUltimosTiempos)
 #	print("leeeo")
+
+
 func datosPartida():	# leer datos actuales de partida 
 	cocheSeleccionado = coche.get_child(0).get_name() #paguardar nombre circuito
 	circuitoSeleccionado = Global.pista #guardar nobre ccoche
 	fechaPartida = OS.get_date(true) #guardar fecha partida
-	diccionarioPartida  = {"coche": cocheSeleccionado,"circuito": circuitoSeleccionado,"fecha": fechaPartida}
+	diccionarioPartida  = {"coche": cocheSeleccionado,"circuito": circuitoSeleccionado,"fecha": fechaPartida, "tiempoVuelta": tiempoVuelta}
 	#guardopartida datos actuales de partida 
-	diccionarioPaGuardar = {"key1": diccionarioPartida}
+	
+	diccionarioPaGuardar.partida = diccionarioPartida
 #	imprimeInfo.set_text("Partida anterior " + str(diccionarioUltimaPartida.fecha.day) + "-" + str(diccionarioUltimaPartida.fecha.month)+"-"+ str(diccionarioUltimaPartida.fecha.year)+ " Circuito: " + str(diccionarioUltimaPartida.circuito) + " Coche: " + str(diccionarioUltimaPartida.coche))
 	if not archivoLeer.file_exists(direccionArchivo):
 		imprimeInfo.set_text("Bienvenido" )
@@ -198,4 +280,23 @@ func datosPartida():	# leer datos actuales de partida
 		imprimeInfo.set_text("Partida anterior " + str(diccionarioUltimaPartida.fecha.day) + "-" + str(diccionarioUltimaPartida.fecha.month)+"-"+ str(diccionarioUltimaPartida.fecha.year)+ " Circuito: " + str(diccionarioUltimaPartida.circuito) + " Coche: " + str(diccionarioUltimaPartida.coche)+" tiempo: ")
 	
 	imprimePartidaActual.set_text("Partida actual " + str(diccionarioPartida.fecha.day)+ "-" + str(diccionarioPartida.fecha.month)+"-"+ str(diccionarioPartida.fecha.year)+ " Circuito: " + str(diccionarioPartida.circuito) + " Coche: " + str(diccionarioPartida.coche))
+
+func grabarTiempos():
+	if fantasmika.vueltas > vueltas:
+		tiempoVuelta = segundosT
+		vueltas += 1
+		diccionarioTiempos.Tvuela = tiempoVuelta
+		diccionarioPaGuardar.tiempos = diccionarioTiempos
+		guardarJuego(diccionarioPaGuardar)
+		print("meto paso por meta, tiempo de vuelta: " + str(tiempoVuelta) +" segundos." )
+		if BanderaMarcador == false:
+			print("saca el cartel último tiempo")
+			MarcadoTiempo = preEsceMarcador.instance()
+			get_parent().add_child(MarcadoTiempo)
+			MarcadoTiempo.set_pos(Vector2(0,200))
+			BanderaMarcador = true
+			InicioTempo3s = segundosT
+			MarcadoTiempo.get_node("Informacion").set_text("TV: "+str(tiempoVuelta)+" s")
+	
+	
 	
